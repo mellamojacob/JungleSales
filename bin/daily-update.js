2f83e47af2172fe482cf7438863812e4cc17ab33
mellamojacob <development.virgin@gmail.com>
2017-11-29 14:48:12 -0700

    Moved daily update to bin folder

---
+++ bin/daily-update.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+
+var Company = require('../models/company.'),
+
+
+Company.all(function(companies) {
+	for(var company in companies) {
+		var time = companies[company].time_stamp;
+		var id = companies[company]._id;
+		var name = companies[company].name;
+		var data;
+		if(time != undefined) {
+			time = time - 1;
+			if(time == 1000) {
+				time = 7;
+			}
+			data = {time_stamp: time};
+			Company.update(name, data);
+			console.log(name, " was updated");
+		}
+	}
+});
--- controllers/companies.js
+++ controllers/companies.js
@@ -19,24 +19,6 @@ router.get('/:id', function(req, res) {
 	});
 });
 
-var j = schedule.scheduleJob('0 20 14 * * *', function() {
-	var allComps = Company.all(function(companies) {
-		for(var company in companies) {
-			//console.log("The company timestamp is ", companies[company].time_stamp);
-			var time = companies[company].time_stamp;
-			var id = companies[company]._id;
-			var name = companies[company].name;
-			var data;
-			if(time != undefined) {
-				time = time - 1;
-				if(time == 1000) {
-					time = 7;
-				}
-				data = {time_stamp: time};
-				Company.update(name, data);
-				console.log(name, " was updated");
-			}
-		}});
-	});
+
 
 module.exports = router
